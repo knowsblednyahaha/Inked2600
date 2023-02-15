cdcc03ff599a765327c711d1d1c5d0a65c432c8a
dzyrick2 <daniel.gayao7@gmail.com>
2023-02-15 11:59:43 +0800

    cursor animation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,54 @@
 import {Route, Routes} from 'react-router-dom'
+import { useEffect, useState } from "react";
 
 import {Homepage} from './Pages/Homepage';
 import {Artistpage} from './Pages/Artistpage'
 
 import './app.scss'
+import { motion } from "framer-motion";
 
 
 function App() {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0
+  });
+  const [cursorVariant, setCursorVariant] = useState("default");
+
+
+  useEffect(() => {
+    const mouseMove = (e: { clientX: any; clientY: any; }) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY
+      })
+    }
+
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    }
+  }, []);
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 16,
+      y: mousePosition.y - 16,
+    },
+    text: {
+      height: 150,
+      width: 150,
+      x: mousePosition.x - 75,
+      y: mousePosition.y - 75,
+      backgroundColor: "yellow",
+      mixBlendMode: "difference"
+    }
+  }
+
+  // const textEnter = () => setCursorVariant("text");
+  // const textLeave = () => setCursorVariant("default");
+
   return (
     <>
       <Routes>
@@ -14,6 +56,11 @@ function App() {
         <Route path='/artist' element={<Artistpage />} />
         
       </Routes>
+      <motion.div
+        className='cursor'
+        variants={variants}
+        animate={cursorVariant}
+      />
     </>
   )
 }
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import Cursor from "./cursor";
+import { gsap } from "gsap";
+
+const body = document.querySelector("body");
+
+window.onload = () => {
+    const cursor = new Cursor(document.querySelector(".cursor"));
+};
