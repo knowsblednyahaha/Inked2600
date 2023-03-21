09569efc52cdc5262399c3fb503941a7adb88ef6
dzyrick2 <daniel.gayao7@gmail.com>
2023-03-21 23:37:12 +0800

    reverted code removed locomotive-scroll

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -7,18 +7,18 @@ import ContactHero from '../Components/Contact-Hero';
 import Footer from '../Components/Footer';
 import CursorElement from '../Cursor/CursorElement';
 
-import {useEffect} from 'react';
-import LocomotiveScroll from 'locomotive-scroll';
+// import {useEffect} from 'react';
+// import LocomotiveScroll from 'locomotive-scroll';
 // import 'locomotive-scroll/src/locomotive-scroll.scss'
 
 
 export const Homepage = () => {
-  useEffect( () => {
-    new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]') as any,
-    smooth: true
-    });
-}, []);
+//   useEffect( () => {
+//     new LocomotiveScroll({
+//     el: document.querySelector('[data-scroll-container]') as any,
+//     smooth: true
+//     });
+// }, []);
   return (
     <div data-scroll-container>
       <Navbar/>
