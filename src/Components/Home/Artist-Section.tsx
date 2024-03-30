8cebb48537c8dc7be6f82880b97864fa3346608e
dzyrick2 <daniel.gayao7@gmail.com>
2024-03-30 13:58:26 +0800

    fixed layout of every section

--- src/Components/Home/Artist-Section.tsx
+++ src/Components/Home/Artist-Section.tsx
@@ -61,7 +61,7 @@ function ArtistSection() {
             spaceBetween={10}
             slidesPerView={1}
             autoplay={{
-              delay: 5000,
+              delay: 5000000,
               disableOnInteraction: false,
             }}
           >
